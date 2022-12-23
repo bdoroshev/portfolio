d8f7273136a55b0ce68044c2e4f6aa8169520858
Bogdan <upwork@landingpage.com>
2022-12-23 16:10:49 +0100

    true glue project added

--- data/projects.ts
+++ data/projects.ts
@@ -2,12 +2,20 @@ import { IProject } from '../models'
 
 export const projects: IProject[] = [
 	{
+		url: 'https://www.true-glue.com/',
+		image: '/true-glue.jpg',
+		title: 'True Glue',
+    description: 'Get the best of both worlds: Performance-packed makeup with no harsh chemicals. Experience clean beauty that acts dirty.',
+		scope: 'Design website in Figma and develop custom Shopify theme',
+		role: 'Designer and Senior Shopify Developer'
+	},
+	{
 		url: 'https://pawpantry.com.au/',
 		image: '/pawpantry.jpg',
 		title: 'Paw Pantry',
     description: 'Paw Pantry a one-stop-shop born to provide everything you need in pet care. Paw Pantry here to help your pet live a happier and healthier life.',
 		scope: 'Develop website from Figma design to custom Shopify theme',
-		role: 'Shopify Developer part of Vine Digital team'
+		role: 'Senior Shopify Developer part of Vine Digital team'
 	},
 	{
 		url: 'https://simplyroastedcrisps.co.uk/',
@@ -15,7 +23,7 @@ export const projects: IProject[] = [
 		title: 'Simply Roasted',
     description: 'Simply Roasted here to change the crisps we eat, for the better, forever. that’s a big claim, but we believe we’ve found a better way to make crisps, with everything you want (taste, crunch, satisfaction) and less of what you don’t want (calories, fat, salt).',
 		scope: 'Develop website from Figma design to custom Shopify theme',
-		role: 'Shopify Developer part of Superco team'
+		role: 'Senior Shopify Developer part of Superco team'
 	},
 	{
 		url: 'https://vitalitycycles.com/',
@@ -23,15 +31,15 @@ export const projects: IProject[] = [
 		title: 'Vitality Cycles',
     description: 'Custom formulated by Dr Jennifer Daniels, Vitality Capsules are infused with natural ingredients such as garlic, ginger, and aloe, our vitality capsules are vital to maintaining a healthy and active life.',
 		scope: 'Develop website from Figma design to custom Shopify theme',
-		role: 'Shopify Developer'
+		role: 'Senior Shopify Developer'
 	},
 	{
 		url: 'https://www.zendxb.com/',
 		image: '/zendxb.jpg',
 		title: 'Zendxb',
     description: 'Dubai’s first home delivered STI test and treatment service. Courier delivery and pickup service. State of the art molecular laboratory testing. Results delivered to your mobile in under 3 hours with VIP processing.',
-		scope: 'Design website in Figma and develop on Shopify',
-		role: 'Designer and Shopify Developer'
+		scope: 'Design website in Figma and develop custom Shopify theme',
+		role: 'Designer and Senior Shopify Developer'
 	},
 	{
 		url: 'https://thesimpleroot.co.uk/',
@@ -39,6 +47,6 @@ export const projects: IProject[] = [
 		title: 'The Simple Root',
     description: 'Everything we do starts with simple root veg. In fact, its our #1 ingredient. We take British grown potatoes, parsnips & sweet potatoes, and with a little thought and care, turn them into the creamiest sauces, dips and cheeses - no dairy needed.',
 		scope: 'Develop website from Figma design to custom Shopify theme',
-		role: 'Shopify Developer part of Superco team'
+		role: 'Senior Shopify Developer part of Superco team'
 	}
 ]
